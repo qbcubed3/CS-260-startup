908cc3a8fcc7d9668b51b5ceba6331ffcb3a9f5c
Ben Christensen <benjaminb.christensen@outlook.com>
2024-02-28 01:17:15 -0700

    login done

---
+++ index.js
@@ -0,0 +1,15 @@
+document.getElementById("login").addEventListener('click', login);
+
+function login() {
+    console.log("is this even running");
+    var password = document.getElementById("password").value;
+    var username = document.getElementById("username").value;
+
+    var passText = password.value;
+    var userText = username.value;
+
+    password.value = "";
+    username.value = "";
+
+    window.location.href = "survey.html";
+}
