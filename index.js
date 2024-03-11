e0a23201b2a518cc49b04b1669563265e1ee5651
Ben Christensen <benjaminb.christensen@outlook.com>
2024-03-11 12:32:33 -0600

    node.js started

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const express = require('express');
+const app = express();
+
+const port = process.argv.length > 2 ? process.argv[2] : 3000;
+
+app.use(express.json());
+
+app.use(express.static('public'));
+
 document.getElementById("login").addEventListener('click', login);
 
+//logs in a user
 function login() {
     console.log("is this even running");
     var password = document.getElementById("password").value;
