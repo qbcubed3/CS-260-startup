067582c570687e09885d36fca82def5f122404f3
Ben Christensen <benjaminb.christensen@outlook.com>
2024-03-11 15:52:36 -0600

    joke endpoint added

--- public/homepage.js
+++ public/homepage.js
@@ -1,20 +1,3 @@
-document.getElementById("login").addEventListener('click', login);
-
-//logs in a user
-function login() {
-    console.log("is this even running");
-    var password = document.getElementById("password").value;
-    var username = document.getElementById("username").value;
-
-    var passText = password.value;
-    var userText = username.value;
-
-    password.value = "";
-    username.value = "";
-
-    window.location.href = "survey.html";
-}
-
 async function randomJoke() {
     var joke;
     try{
--- public/stats.js
+++ public/stats.js
@@ -146,3 +146,26 @@ new Chart(chart, {
     }
 });
 
+async function randomJoke() {
+  var joke;
+  try{
+      fetch('https://official-joke-api.appspot.com/jokes/random')
+          .then(response =>{
+              return response.json();
+          })
+          .then(data => {
+              const setup = data.setup;
+              const punchline = data.punchline;
+              document.getElementById("setup").textContent = setup;
+              document.getElementById("punchline").textContent= punchline;
+              console.log(setup + " : " + punchline);
+          })      
+  }
+  catch (error){
+      console.log('bad request')
+      return;
+  }
+  console.log(joke);
+}
+
+randomJoke();
